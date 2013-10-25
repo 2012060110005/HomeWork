a81ae00435943f435c44088fcc8e3be2931f9bca
2012060110005 <tsqingqing@foxmail.com>
2013-10-25 23:42:12 +0800

    温度

---
+++ temperature.cpp
@@ -0,0 +1,20 @@
+// temperature.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	return 0;
+}
+#include<iostream>
+using namespace std;
+int main()
+{
+	double c,f;
+	cin>>f;
+	c=5*(f-32)/9;
+	cout<<f<<"华氏度"<<"="<<c<<"摄氏度";
+    return 0;
+}
